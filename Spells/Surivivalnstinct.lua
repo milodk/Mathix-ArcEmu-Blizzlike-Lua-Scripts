49b3833e0286e50820e458003a3957065e527dff
Emil Due Rasmussen <emildr12@gmail.com>
2013-02-11 17:07:24 +0100

    Fixed survival instincts script

--- Spells/Surivivalnstinct.lua
+++ Spells/Surivivalnstinct.lua
@@ -2,14 +2,15 @@
 -- This script is created by zdroid9770; please do not edit this --
 -- script and claim it as your own, as of All rights are claimed --
 -- by me.                                                        --
---                     Copyright© zdroid9770					 --
+--                     Copyrightï¿½ zdroid9770					 --
+--						  Fixed by Mathix						 --
 -------------------------------------------------------------------
 
 function SurivalInstincts(event, player, SpellId, pSpellObject)
 	if(SpellId == 61336) then
 		local plr = player:GetHealth()
 		local maxhp = player:GetMaxHealth()
-		player:SetHealth(plr * 0.30, maxhp * 0.30)
+		player:SetHealth(plr * 1.30, maxhp * 1.30)
 		player:RegisterTimedEvent("ResetHP", 20000, 1)
 	end
 end
