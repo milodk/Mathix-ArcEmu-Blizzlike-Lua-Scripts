77e0839eb91f0fc43e37b932aca0edca78c6cdf3
Emil Due Rasmussen <emildr12@gmail.com>
2013-02-13 11:00:55 +0100

    Intimidation fixed

--- Spells/Hunter/Intimidation.lua
+++ Spells/Hunter/Intimidation.lua
@@ -1,2 +1,14 @@
 -- Intimidation fix by Mathix of ac-web.org
+function IntimidationFix(event, plr, spellid)
+	if (spellid == 19577) then
+		local pet = tostring(plr)
+		local pettarget = pet:GetSelection
+		
+		if pettarget:IsFriendly() == false then
+			pet:CastSpellOnTarget(19577, pettarget)
+		end
+	end
+end
 
+RegisterServerHook(10, "IntimidationFix")
+RegisterUnitEvent(pet, 13, "
