6186e833d29433842acb047dc950481fc2f37edd
Emil Due Rasmussen <emildr12@gmail.com>
2013-02-14 22:42:45 +0100

    Fan of knives is now fixed.

--- Spells/Rogue/FanOfKnives.lua
+++ Spells/Rogue/FanOfKnives.lua
@@ -1,11 +1,14 @@
 -- Fan of Knives fix by Mathix of ac-web.org
+-- A big thanks to Foereaper for the loop!
 function FanOfKnivesFix(event, plr, spellid)
-	if (spellid == 51723) then
-		Enemys = plr:GetInRangeEnemies()
-		if plr:GetDistanceYards(Enemies) <= 8 then
-			local damage = math.random(250,320)
-			plr:DealDamage(Enemies, damage, 51723)
+    if (spellid == 51723) then
+        local Enemies = plr:GetInRangeEnemies()
+		for _, v in ipairs(Enemies) do
+			if plr:GetDistanceYards(v) <= 8 then
+				plr:DealDamage(v, math.random(400, 500), 51723)
+			end
 		end
 	end
-
+end
+ 
 RegisterServerHook(10, "FanOfKnivesFix")
